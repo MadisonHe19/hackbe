68546a6bff68b56077684331e0d80b6dfc57116b
Dajah Lasenberry <deejay@Dajahs-MacBook-Air.local>
2017-10-08 00:57:37 -0700

    First commit

---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs'
+
+target 'MyTarget' do
+  pod 'TwilioChatClient', '~> 2.0.0'
+end
---
+++ ToyotaAdvancedRetail/Car.swift
@@ -0,0 +1,13 @@
+//
+//  Car.swift
+//  ToyotaAdvancedRetail
+//
+//  Created by Dajah Lasenberry on 10/8/17.
+//  Copyright © 2017 hackathonbe. All rights reserved.
+//
+
+import UIKit
+
+class Car: NSObject {
+
+}
